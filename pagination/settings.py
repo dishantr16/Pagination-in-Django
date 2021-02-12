0b283029a5c5cfc317956e548037f81e0b268ef9
Dishant Rathi <dishant16rathi@gmail.com>
2021-02-12 13:19:51 +0530

    Pagination and records are set

--- pagination/settings.py
+++ pagination/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
-    }
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 5,
+#     }
